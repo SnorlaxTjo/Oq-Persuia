a6909d7ef3b669343d2136733bac8a29cd6fbd03
SnorlaxTjo <casmar1202@icloud.com>
2024-11-10 19:36:36 +0100

    News about attack cutscene
    It was annoying

--- Assets/Scripts/Managers/CutsceneManager.cs
+++ Assets/Scripts/Managers/CutsceneManager.cs
@@ -6,17 +6,30 @@ public class CutsceneManager : MonoBehaviour
 {
     [SerializeField] Cutscene[] cutscenes;
     [SerializeField] TextMeshProUGUI cutsceneText;
+    [SerializeField] GameObject[] cameras;
 
     [SerializeField] Animator animator;
 
+    PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
     public void StartCutscene(int cutsceneToStart)
     {
+        animator.enabled = true;
         animator.SetInteger("AnimationID", cutsceneToStart);
 
         foreach (GameObject world in cutscenes[cutsceneToStart].extraActiveWorlds)
         {
             world.SetActive(true);
         }
+        cameras[0].SetActive(false);
+        cameras[1].SetActive(true);
+
+        playerController.CompleteMoveBlock = true;
     }
 
     public void ChangeText(string text)
@@ -27,6 +40,17 @@ public class CutsceneManager : MonoBehaviour
     public void ResetAnimationId()
     {
         animator.SetInteger("AnimationID", -1);
+        cameras[0].SetActive(true);
+        cameras[1].SetActive(false);
+
+        playerController.CompleteMoveBlock = false;
+        animator.enabled = false;
+    }
+
+    public void ResetCamera()
+    {
+        cameras[0].SetActive(true);
+        cameras[1].SetActive(false);
     }
 }
 
